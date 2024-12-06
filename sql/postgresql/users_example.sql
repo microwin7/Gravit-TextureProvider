6bcb34128e878e4b4d7ace102b5f49b9c63c09ea
microwin7 <usa.microwin8@gmail.com>
2024-12-06 05:31:15 +0300

    Clean docker

--- sql/postgresql/users_example.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE texture_provider_users (
-    id serial4 NOT NULL,
-    username varchar NULL,
-    "uuid" varchar NULL,
-    CONSTRAINT pk PRIMARY KEY (id),
-    CONSTRAINT username_un UNIQUE (username),
-    CONSTRAINT uuid_un UNIQUE (uuid)
-);
